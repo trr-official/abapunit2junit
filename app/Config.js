5d0e26cafcdc0b2c7174ee926381070cb2e888fc
Marcello Urbani <marcello.urbani@gmail.com>
2023-05-28 18:55:44 +0100

    replace request with axios/got Fixes #15

--- app/Config.js
+++ app/Config.js
@@ -1,24 +1,31 @@
-const argv = require("yargs")
-    .describe('host', 'Netweaver Host')
-    .describe('password','Password')
-    .describe('username','Username')
-    .choices('protocol',['http','https'])
-    .describe('protocol',"HTTP or HTTPS")
-    .default('protocol','https')
-    .describe('insecure', "Allow untrusted ssl certificates")
-    .default('insecure',false)
-    .describe('package', 'ABAP Package containing the unit tests')
-    .describe('out', "Output file")
-    .default('out', 'result/output.xml')
-    .describe('aunit', 'Save AUnit Result')
-    .default('aunit', false )
-    .describe('aunitout', "Result from abapunit")
-    .default('aunitout', 'result/abapresult.xml')
-    .demandOption(['host', 'username','password','package'], '').argv;
+const yargs = require("yargs")
+
+const loadArgv = () => {
+    let argv = yargs
+        .describe('host', 'Netweaver Host')
+        .describe('password', 'Password')
+        .describe('username', 'Username')
+        .choices('protocol', ['http', 'https'])
+        .describe('protocol', "HTTP or HTTPS")
+        .default('protocol', process.env.protocol || 'https')
+        .describe('insecure', "Allow untrusted ssl certificates")
+        .default('insecure', false)
+        .describe('package', 'ABAP Package containing the unit tests')
+        .describe('out', "Output file")
+        .default('out', 'result/output.xml')
+        .describe('aunit', 'Save AUnit Result')
+        .default('aunit', false)
+        .describe('aunitout', "Result from abapunit")
+        .default('aunitout', 'result/abapresult.xml')
+    const mandatory = ['host', 'username', 'password', 'package'].filter(k => !process.env[k])
+    if (mandatory.length) argv.demandOption(mandatory, '')
+    return argv.argv
+}
+
 
 var configuration;
 
-function Config(  ) {
+function Config() {
     this.configuration = initialize();
 }
 
@@ -28,30 +35,32 @@ function Config(  ) {
  * Initialize variables needed
  * @param Setting parameters
  */
-function initialize(  ) {
+function initialize() {
+    const argv = loadArgv()
+    const getarg = k => argv[k] || process.env[k]
     const config =
-        {
-            network : {
-                host : argv.host,
-                protocol : argv.protocol, 
-                insecure :  argv.insecure,  
-            },
-            auth : {
-                username : argv.username ,
-                password : argv.password, 
-            },
-            test : {
-                package: argv.package, 
-            },
-            result :{
-                file : argv.out,
-                saveAunit : argv.aunit,
-                abapResultFile : argv.aunitout
-            }
-
+    {
+        network: {
+            host: getarg("host"),
+            protocol: getarg("protocol"),
+            insecure: getarg("insecure"),
+        },
+        auth: {
+            username: getarg("username"),
+            password: getarg("password"),
+        },
+        test: {
+            package: getarg("package"),
+        },
+        result: {
+            file: getarg("out"),
+            saveAunit: getarg("aunit"),
+            abapResultFile: getarg("aunitout")
         }
-        return config; 
-    
+
+    }
+    return config;
+
 }
 
 
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Load dependencies
+const { CookieJar } = require('tough-cookie');
+const { wrapper } = require('axios-cookiejar-support');
 const xsltProcessor = require('xslt-processor');
 const fs = require("fs");
-const rp = require('request-promise').defaults({ jar: true });
+const axios = require("axios")
 const path = require("path");
 const Config = require("./app/Config.js");
 
-
-
+const jar = new CookieJar();
+const client = wrapper(axios.create({ jar }));
 /**
  * Reads XML files needed to run AUnit Tests and transform to JUnit
  * @return xml file with call to run abap unit test, xsl to transform from  AUnit Result to JUnit Result
@@ -14,7 +16,7 @@ const Config = require("./app/Config.js");
     const xsltData = fs.readFileSync(path.resolve(__dirname, "./xml/aunit2junit.xsl"));
     const xmlRunAbapUnitBuffer = fs.readFileSync(path.resolve(__dirname, "./xml/runAbapUnit.xml"));
     const xslt = xsltProcessor.xmlParse(xsltData.toString()); // xsltString: string of xslt file contents
-    const xmlRunAbapUnit = xmlRunAbapUnitBuffer.toString('utf8').replace("{{package}}", config.configuration.test.package ); 
+    const xmlRunAbapUnit = xmlRunAbapUnitBuffer.toString('utf8').replace("{{package}}", config.configuration.test.package);
     return { xmlRunAbapUnit, xslt };
 }
 
@@ -25,24 +27,24 @@ const Config = require("./app/Config.js");
  */
 function runAbapUnitTest(xmlRunAbapUnit, xCSRFToken) {
     const optionsRunUnitTest = getRunUnitTestOptions(xmlRunAbapUnit, xCSRFToken);
-    return rp(optionsRunUnitTest);
+    return client(optionsRunUnitTest);
 }
 
-function getRunUnitTestOptions(xmlRunAbapUnit, 
-    xCSRFToken) {
+function getRunUnitTestOptions(xmlRunAbapUnit, xCSRFToken) {
     return {
         method: 'POST',
         url: config.configuration.network.protocol + '://' + config.configuration.network.host + '/sap/bc/adt/abapunit/testruns',
         auth: {
-            user: config.configuration.auth.username,
+            username: config.configuration.auth.username,
             password: config.configuration.auth.password
         },
+        responseType: "text",
         headers: {
             'x-csrf-token': xCSRFToken,
             'Content-Type': "application/vnd.sap.adt.abapunit.testruns.config.v2+xml",
-            'Accept' : 'application/vnd.sap.adt.abapunit.testruns.result.v1+xml'
+            'Accept': 'application/vnd.sap.adt.abapunit.testruns.result.v1+xml'
         },
-        body: xmlRunAbapUnit,
+        data: xmlRunAbapUnit,
         insecure: config.configuration.network.insecure,
         rejectUnauthorized: !config.configuration.network.insecure
     };
@@ -51,24 +53,24 @@ function getRunUnitTestOptions(xmlRunAbapUnit,
 /** Get CSRF Token by calling GET with x-csrf-token: fetch 
  * @returns Promise with the result of the call
 */
-function getCSRFToken() {
+async function getCSRFToken() {
     const optionsGetCSRFToken = getCSRFTokenOptions();
-    return rp(optionsGetCSRFToken);
+    const resp = await client(optionsGetCSRFToken);
+    return resp
 }
 
 function getCSRFTokenOptions() {
     return {
         method: "GET",
-        url: config.configuration.network.protocol + '://' + config.configuration.network.host + '/sap/bc/adt/abapunit/testruns',
+        url: config.configuration.network.protocol + '://' + config.configuration.network.host + '/sap/bc/adt/compatibility/graph',
         simple: false,
         resolveWithFullResponse: true,
         auth: {
-            user: config.configuration.auth.username,
+            username: config.configuration.auth.username,
             password: config.configuration.auth.password
         },
-        headers: {
-            'X-CSRF-Token': 'fetch'
-        },
+        responseType: "text",
+        headers: { 'X-CSRF-Token': 'fetch' },
         insecure: config.configuration.network.insecure,
         rejectUnauthorized: !config.configuration.network.insecure
     };
@@ -83,12 +85,12 @@ function main() {
     config = new Config();
 
     const { xmlRunAbapUnit, xslt } = readXml();
-    
+
     csrfTokenPromise = getCSRFToken();
 
     var runAbapUnitTestPromise = csrfTokenPromise.then(function (response) {
         var csrfToken = response.headers['x-csrf-token'];
-        return runAbapUnitTest(xmlRunAbapUnit,csrfToken);
+        return runAbapUnitTest(xmlRunAbapUnit, csrfToken);
     }
     ).catch(function (err) {
         console.error("ERROR: " + JSON.stringify(err));
@@ -96,9 +98,9 @@ function main() {
     );
 
     runAbapUnitTestPromise.then(function (parsedBody) {
-        const xml = xsltProcessor.xmlParse(parsedBody); // xsltString: string of xslt file contents
-        if( config.configuration.result.saveAunit ) {
-            fs.writeFileSync(config.configuration.result.abapResultFile,parsedBody);
+        const xml = xsltProcessor.xmlParse(parsedBody.data); // xsltString: string of xslt file contents
+        if (config.configuration.result.saveAunit) {
+            fs.writeFileSync(config.configuration.result.abapResultFile, parsedBody);
         }
         const outXmlString = xsltProcessor.xsltProcess(xml, xslt); // outXmlString: output xml string.
         fs.writeFileSync(config.configuration.result.file, outXmlString)
