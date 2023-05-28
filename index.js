5f29c8777de4a79f1f9e4bf14398534ccf8afff7
Marcello Urbani <marcello.urbani@gmail.com>
2023-05-28 19:22:12 +0100

    refactor, add self signed support

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const fs = require("fs");
 const axios = require("axios")
 const path = require("path");
 const Config = require("./app/Config.js");
+const https = require('https');
 
 const jar = new CookieJar();
 const client = wrapper(axios.create({ jar }));
 /**
  * Reads XML files needed to run AUnit Tests and transform to JUnit
  * @return xml file with call to run abap unit test, xsl to transform from  AUnit Result to JUnit Result
- */function readXml() {
+ */function readXml(config) {
     const xsltData = fs.readFileSync(path.resolve(__dirname, "./xml/aunit2junit.xsl"));
     const xmlRunAbapUnitBuffer = fs.readFileSync(path.resolve(__dirname, "./xml/runAbapUnit.xml"));
     const xslt = xsltProcessor.xmlParse(xsltData.toString()); // xsltString: string of xslt file contents
@@ -25,54 +26,52 @@ const client = wrapper(axios.create({ jar }));
  * @param csrf token needed for the call
  * @returns Promise with the result
  */
-function runAbapUnitTest(xmlRunAbapUnit, xCSRFToken) {
-    const optionsRunUnitTest = getRunUnitTestOptions(xmlRunAbapUnit, xCSRFToken);
+function runAbapUnitTest(xmlRunAbapUnit, xCSRFToken, config) {
+    const optionsRunUnitTest = getRunUnitTestOptions(xmlRunAbapUnit, xCSRFToken, config);
     return client(optionsRunUnitTest);
 }
 
-function getRunUnitTestOptions(xmlRunAbapUnit, xCSRFToken) {
+const commonOptions = config => {
+    const opts = {
+        auth: config.configuration.auth,
+        responseType: "text"
+    }
+    if (config.configuration.network.insecure) opts.httpsAgent = new https.Agent({
+        keepAlive: true,
+        rejectUnauthorized: false
+    })
+    return opts
+}
+
+function getRunUnitTestOptions(xmlRunAbapUnit, xCSRFToken, config) {
     return {
+        ...commonOptions(config),
         method: 'POST',
         url: config.configuration.network.protocol + '://' + config.configuration.network.host + '/sap/bc/adt/abapunit/testruns',
-        auth: {
-            username: config.configuration.auth.username,
-            password: config.configuration.auth.password
-        },
-        responseType: "text",
         headers: {
             'x-csrf-token': xCSRFToken,
             'Content-Type': "application/vnd.sap.adt.abapunit.testruns.config.v2+xml",
             'Accept': 'application/vnd.sap.adt.abapunit.testruns.result.v1+xml'
         },
-        data: xmlRunAbapUnit,
-        insecure: config.configuration.network.insecure,
-        rejectUnauthorized: !config.configuration.network.insecure
+        data: xmlRunAbapUnit
     };
 }
 
 /** Get CSRF Token by calling GET with x-csrf-token: fetch 
  * @returns Promise with the result of the call
 */
-async function getCSRFToken() {
-    const optionsGetCSRFToken = getCSRFTokenOptions();
+async function getCSRFToken(config) {
+    const optionsGetCSRFToken = getCSRFTokenOptions(config);
     const resp = await client(optionsGetCSRFToken);
-    return resp
+    return resp.headers['x-csrf-token']
 }
 
-function getCSRFTokenOptions() {
+function getCSRFTokenOptions(config) {
     return {
+        ...commonOptions(config),
         method: "GET",
         url: config.configuration.network.protocol + '://' + config.configuration.network.host + '/sap/bc/adt/compatibility/graph',
-        simple: false,
-        resolveWithFullResponse: true,
-        auth: {
-            username: config.configuration.auth.username,
-            password: config.configuration.auth.password
-        },
-        responseType: "text",
-        headers: { 'X-CSRF-Token': 'fetch' },
-        insecure: config.configuration.network.insecure,
-        rejectUnauthorized: !config.configuration.network.insecure
+        headers: { 'X-CSRF-Token': 'fetch' }
     };
 }
 
@@ -81,32 +80,23 @@ function getCSRFTokenOptions() {
 * 2) Call Netweaver Server and get abap unit results
 * 3) Transform result and save to output.xml
 **/
-function main() {
-    config = new Config();
+async function main() {
 
-    const { xmlRunAbapUnit, xslt } = readXml();
-
-    csrfTokenPromise = getCSRFToken();
-
-    var runAbapUnitTestPromise = csrfTokenPromise.then(function (response) {
-        var csrfToken = response.headers['x-csrf-token'];
-        return runAbapUnitTest(xmlRunAbapUnit, csrfToken);
-    }
-    ).catch(function (err) {
-        console.error("ERROR: " + JSON.stringify(err));
-    }
-    );
-
-    runAbapUnitTestPromise.then(function (parsedBody) {
-        const xml = xsltProcessor.xmlParse(parsedBody.data); // xsltString: string of xslt file contents
-        if (config.configuration.result.saveAunit) {
+    try {
+        const config = new Config();
+        const { xmlRunAbapUnit, xslt } = readXml(config);
+        const token = await getCSRFToken(config);
+        const test = await runAbapUnitTest(xmlRunAbapUnit, token, config)
+        const xml = xsltProcessor.xmlParse(test.data); // xsltString: string of xslt file contents
+        if (config.configuration.result.saveAunit)
             fs.writeFileSync(config.configuration.result.abapResultFile, parsedBody);
-        }
         const outXmlString = xsltProcessor.xsltProcess(xml, xslt); // outXmlString: output xml string.
         fs.writeFileSync(config.configuration.result.file, outXmlString)
-    }).catch(function (err) {
-        console.error(err);
-    });
+
+    } catch (err) {
+        console.error("ERROR: " + err.message || JSON.stringify(err));
+        process.exit(1)
+    }
 }
 
 main();
